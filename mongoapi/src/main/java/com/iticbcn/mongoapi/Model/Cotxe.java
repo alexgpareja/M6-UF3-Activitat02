e0335322b587c34c75ca6a1042c3248707ec39a5
Alex Gil <alexgpareja@icloud.com>
2025-05-16 17:19:31 +0200

    model cotxe afegit

---
+++ mongoapi/src/main/java/com/iticbcn/mongoapi/Model/Cotxe.java
@@ -0,0 +1,28 @@
+package com.iticbcn.mongoapi.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document(collection = "cotxes")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Cotxe implements Serializable {
+    
+    @Id
+    private String id;
+    private String marca;
+    private String model;
+    private int any;
+    private double preu;
+    private String color;
+}
