0bcee084b8e769752a156d27b49ce6c2143e86d5
Dani <thos.merida.dani@gmail.com>
2025-05-16 17:27:11 +0200

    cambios en el repository

--- mongoapi/src/main/java/com/iticbcn/mongoapi/Repository/CotxeRepository.java
+++ mongoapi/src/main/java/com/iticbcn/mongoapi/Repository/CotxeRepository.java
@@ -1,9 +1,13 @@
 package com.iticbcn.mongoapi.Repository;
 
 import com.iticbcn.mongoapi.Model.Cotxe;
+
+import reactor.core.publisher.Flux;
+
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CotxeRepository extends ReactiveMongoRepository<Cotxe, String> {
+    Flux<Cotxe> findByMarca(String marca);
 }
